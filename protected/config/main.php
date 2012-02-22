188d147d019a2327f1fd562b1d7eadf041909061
Elvan <evanhd@live.com>
2012-02-22 23:53:21 +0700

    Added bootstrap theme

--- protected/config/main.php
+++ protected/config/main.php
@@ -11,6 +11,7 @@ return array(
 
 	// preloading 'log' component
 	'preload'=>array('log'),
+  'theme' => 'classic',
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -36,18 +37,20 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
-		/*
+
+    // uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+      'showScriptName' => false,
 			'rules'=>array(
+        '/' => 'site/index',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
+
+    'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
---
+++ themes/classic/views/layouts/main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap-responsive.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/main.css" />
+  </head>
+  <body>
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <?php echo CHtml::link(CHtml::encode(Yii::app()->name), array('/site/index'), array('class' => 'brand')); ?></a>
+          <div class="nav-collapse">
+            <?php
+            $this->widget('zii.widgets.CMenu', array(
+              'items' => array(
+                array('label' => 'Home', 'url' => array('/site/index')),
+                array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                array('label' => 'Contact', 'url' => array('/site/contact')),
+                array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                array('label' => 'Logout ('.Yii::app()->user->name.')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+              ),
+              'htmlOptions' => array('class' => 'nav'),
+            ));
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <?php echo $content; ?>
+      <hr>
+      <div id="footer">Copyright &copy; <?php echo date('Y'); ?> by My Company. All Rights Reserved. <?php echo Yii::powered(); ?></div>
+    </div>
+  </body>
+</html>
